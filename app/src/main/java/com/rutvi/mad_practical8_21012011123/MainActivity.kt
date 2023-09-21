2a15b583a9facb3979f7efd2194329bed67dc3ff
RUTVI <rutvi162004@gmail.com>
2023-09-21 21:46:28 +0530

    Practical8

--- app/src/main/java/com/rutvi/mad_practical8_21012011123/MainActivity.kt
+++ app/src/main/java/com/rutvi/mad_practical8_21012011123/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.rutvi.mad_practical8_21012011123
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cardView = findViewById<MaterialCardView>(R.id.card2)
+        val createAlarm = findViewById<MaterialButton>(R.id.button_Alarm)
+        createAlarm.setOnClickListener {
+            //cardView.visibility=View.VISIBLE
+            TimePickerDialog(this, { tp, hour, minute -> setAlarmTime(hour, minute) }, 11, 0, false).show()
+        }
+        }
+
+    fun setAlarmTime(hour:Int,minute:Int)
+    {
+        //cardView.visibility=View.GONE
     }
 }
